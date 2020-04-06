7ddb2062d768b5771d7e023895adb42315c89293
DEVA <vasu.seelamneni@gmail.com>
2020-04-07 01:43:38 +0800

    Added and Modified Unit1Exercise.

--- java8features/src/main/java/com/vasu/java8practice/Unit1Excercise.java
+++ java8features/src/main/java/com/vasu/java8practice/Unit1Excercise.java
@@ -16,23 +16,39 @@ public class Unit1Excercise {
 
 
         //Step 1: Sort list by last name
+        getSortByLastName(people);
+
+        //Step 2: Create a method that prints all elements in the list
+        printAllElements(people);
+
+        /*Have the methods accept behaviour instead hard-coding them. For example,
+         pass in behaviour to find if a person's last name begins with 'C'*/
+        //Step 3: Create a method that prints all people that have last name beginning with C
+        printElementsByLastNameStartsWithC(people);
+
+    }
+
+    private static void getSortByLastName(List<Person> people) {
         System.out.println(people);
+
         Collections.sort(people, new PersonComparator());
         System.out.println(people);
+
         Collections.sort(people, new Comparator<Person>() {
-                    @Override
-                    public int compare(Person o1, Person o2) {
-                        Person p1 = (Person) o1;
-                        Person p2 = (Person) o2;
+            @Override
+            public int compare(Person o1, Person o2) {
+                Person p1 = (Person) o1;
+                Person p2 = (Person) o2;
 
-                        if(p1.getLastName().compareTo(p2.getLastName()) != 0){
-                            return p1.getLastName().compareTo(p2.getLastName());
-                        }
+                if(p1.getLastName().compareTo(p2.getLastName()) != 0){
+                    return p1.getLastName().compareTo(p2.getLastName());
+                }
 
-                        return 0;
-                    }
-                });
+                return 0;
+            }
+        });
         System.out.println(people);
+
         Collections.sort(people, ((o1, o2) -> {
             Person p1 = (Person) o1;
             Person p2 = (Person) o2;
@@ -43,25 +59,24 @@ public class Unit1Excercise {
 
             return 0;
         }));
+        System.out.println(people);
+    }
 
-        //Step 2: Create a method that prints all elements in the list
-
+    private static void printAllElements(List<Person> people) {
         Iterator<Person> iterator = people.iterator();
         while (iterator.hasNext()) {
             Person person = iterator.next();
             System.out.println(person);
         }
+    }
 
-        /*Have the methods accept behaviour instead hard-coding them. For example,
-         pass in behaviour to find if a person's last name begins with 'C'*/
-        //Step 3: Create a method that prints all people that have last name beginning with C
+    private static void printElementsByLastNameStartsWithC(List<Person> people) {
         Iterator<Person> iterator1 = people.iterator();
         while (iterator1.hasNext()) {
             Person person = iterator1.next();
             if(person.getLastName().startsWith("C"))
-            System.out.println("---" + person);
+                System.out.println("---" + person);
         }
-
     }
 
     static class PersonComparator implements Comparator {
